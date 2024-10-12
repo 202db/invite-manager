c7d0c79e4e381265ccaf5b1223bb3e33ff8bae80
202.sql <zetfixtiktok@gmail.com>
2024-10-12 21:15:19 +0200

    Add files via upload

---
+++ README.md
@@ -0,0 +1,17 @@
+
+**Commencer**  
+Invite Manager est un Bot Discord avancé, facile à configurer, gratuit. Les instructions pour le configurer sont disponibles [ici] (https://github.com/202db/invite-manager.wiki.git)
+
+**Soutien**  
+Si vous avez besoin d'aide avec le bot, n'hésitez pas à [rejoindre le support](https://discord.gg/WzQGA8A5).
+
+**Autres informations**  
+Invite Manager nécessite Node.js v12 ou supérieur.   
+
+
+Veuillez noter que nous ne sommes pas responsables si quelque chose arrive à votre compte bot. Il est de votre responsabilité de garder le token de votre bot hors de portée de toute personne en qui vous n'avez pas confiance. Le bot utilise discord.js pour interagir avec l'API Discord avec votre token de bot. Ne partagez votre fichier .env avec personne une fois rempli.
+
+Veuillez également noter que Discord a un TOS pour son API : https://discord.com/developers/docs/legal
+
+Ce TOS est mis à jour le 15 août 2020 et exigera que tous les robots aient une politique de confidentialité (même s'il indique simplement que vous ne collectez pas d'informations personnelles et que vous disposez d'un moyen de vous contacter en cas de problème de confidentialité). Vous devrez l'ajouter vous-même.
+Il est de votre responsabilité de vous assurer que votre utilisation de ce bot n'est pas contraire aux conditions d'utilisation des services qu'il utilise.
---
+++ config.js
@@ -0,0 +1,29 @@
+module.exports = {
+    prefix: "/", // Préfix du bot
+    welcomeChannel: "ID", // L'id du salon de bienvenue
+    slashCommands: true, // Activer ou pas les commandes en slash
+    colors: {
+        main: [0, 110, 255],
+        error: [231, 76, 60]
+    },
+    botstatus: {
+        enabled: true, // Activer ou pas le status du bot
+        status: "dnd", // Le status du bot (dnd, online, idle, invisible)
+        activity_type: "streaming", // Le type d'activiter du bot (watching, listening, playing, streaming)
+        activity_text: "CustomPresence", // Le texte sur l'activiter du bot
+        activity_url: "URL" // URL d'un de votre twitch ou youtube
+    },
+    inviteRewards: true, // Active ou pas les récompenses d'invitation sur le serveur
+    rewards: [
+        {
+            invitesNeeded: 5, // Nombre d'inviations réquise
+            roleID: "RoleID" // ID du rôle récompenses
+        }, //Copiez et collez ceci pour chaque récompense d'invitation
+        {
+            invitesNeeded: 10,
+            roleID: "RoleID"
+        }
+    ],
+    welcomeMessage: "{member} a rejoint le serveur il a était inviter par **{inviter}** (qui est maintenant à {invites} invites).", // Utilisez {inviter} pour qui a invité le membre, {member} pour le membre, {code} pour l'invitation utilisée, {mention} pour mentionner l'invitant, {ID} pour l'ID du membre, {inviterID} pour le l'ID de l'invitant et {invites} pour les invitations de l'invitant
+    leaveMessage: "{member} a quitté le serveur, il avais était invité par **${inviter}** (qui est maintenant à ${invites} invites).", // Utilisez {inviter} pour qui a invité le membre, {member} pour le membre, {mention} pour mentionner l'invitant, {ID} pour l'ID du membre, {inviterID} pour l'ID de l'invitant et {invites} pour les invitations de l'invitant
+}
